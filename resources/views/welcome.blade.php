401fc26a2cb882fa7f680ef3de5af80ccef05fa9
ChiaHeung <D200262C@sc.edu.my>
2022-05-22 17:17:20 +0800

    MainPage add Inspiration/ideas + fonts shadow design

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
    <div class="col-md-4">
    <img class="logomain" src="images/logo.png" alt="logo">
    <h1 class="mb-4">Movie Moment (M&M)</h1>
-   <button type="button" class="buttonmain">Explore More</button>
+   <button type="button" class="buttonmain"><a href="#about">Explore More </a></button>
    <hr>  
    </div>
    </div>
@@ -29,9 +29,9 @@
            <div class=col-md-10>
             <div class="row justify-content-center align-items center">
                <div class="col-md-6 text-center justify-content-center align-items center "> 
-                  <h2>Project Teams</h2>
+                  <h2 class="my-2">Project Teams</h2>
                   <p>
-                  Our small teams made up of three members which is Loh Chia Heung, Teh Jun Yuan, and Fang Xiang Lun. Three of us are students which studied Diploma in Information Technology in Southern University College.
+                  Our small teams made up of three members which is Loh Chia Heung, Teh Jun Yuan, and Fang Xiang Lun. Three of us are students who studied Diploma in Information Technology in Southern University College.
                   </p>
                   </div>
                   <div class="col-md-6"><img src="images/projectteam.jpg" alt="projectTeam" class="img-fluid rounded-pill hover-shadow"></div>
@@ -41,7 +41,7 @@
                <div class="col-md-6"><img src="images/objectives.jpg" alt="objectivesGoal" class="img-fluid rounded-pill hover-shadow"></div>
                
                <div class="col-md-6 text-center"> <br><br>
-                  <h2>Objectives/Goals</h2>
+                  <h2 class="my-2">Objectives/Goals</h2>
                   <p>
                   1) Provide a platform for users to share their views on movies with friends. <br>
                   2) Provide movie recommendations for users to occupy their free time. <br>
@@ -52,11 +52,12 @@
             <br>
             <div class="row justify-content-center align-items center">
                <div class="col-md-6 text-center"> <br><br>
-                  <h2>Our Services</h2>
+                  <h2 class="my-2">Our Services</h2>
                   <p>
                   1) Movie Community       - A place for users to chitchatting about movies with each others.<br>
                   2) Movie Recommendation  - A place for users to search for their favorite movies.  <br>
                   3) Top 10 Movie (Weekly) - A place for users to look for highly rated movies.<br>
+                  To be continued...
                   </p>
                   </div>
                   <div class="col-md-6"><img src="images/picture2.jpg" class="img-fluid rounded-pill hover-shadow"></div> 
@@ -65,7 +66,7 @@
             <div class="row justify-content-center align-items center">
                <div class="col-md-6"><img src="images/socialmedia.jpg" alt="socialmediapicture" class="img-fluid rounded-pill hover-shadow"></div>  
                <div class="col-md-6  text-center"> <br><br>
-                  <h2>Social Media Account</h2>
+                  <h2 class="my-2">Social Media Account</h2>
                   <p>We will post the latest updates of our movie recommendation system in the following social media. Please follow us to get the latest news!
                   <br>Follow us on:<br>
                   1.<a class="links" href="https://www.facebook.com/Movie-Moment-MM-109810318198221">Facebook <i class="fab fa-facebook-f"></i></a>
@@ -75,6 +76,19 @@
                   </div>
             </div>
             <br>
+            <div class="row justify-content-center align-items center">
+               <div class="col-md-6 text-center"> <br><br>
+                  <h2 class="my-2">Ideas/Inspiration</h2>
+                  <p>
+                  The inspiration of the name is came form a popular application in China, which is WeChat.
+                  WeChat provide a platfrom for users to share their status, pictures, videos, with their friends and it is called as 
+                  WeChat Moments.Our system also emphasizes in providing a platform for movie lovers to share their views about movies with each other. Therefore
+                  we named is as Movie Moment (M&M).
+                  </p>
+                  </div>
+                  <div class="col-md-6"><img src="images/ideas.jpg" class="img-fluid rounded-pill hover-shadow"></div> 
+               </div><br>
+            </div><br>
          </div>
     </main>
 
