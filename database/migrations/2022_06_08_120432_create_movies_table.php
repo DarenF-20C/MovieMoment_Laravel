9fd8f735c5ad8a04d83da0912bb9122c6ee7393d
TehJunYuan <80732651+TehJunYuan@users.noreply.github.com>
2022-06-12 15:50:59 +0800

    movie category user db update

--- database/migrations/2022_06_08_120432_create_movies_table.php
+++ database/migrations/2022_06_08_120432_create_movies_table.php
@@ -13,14 +13,12 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('movies', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->nullable();
-            $table->string('is_admin')->default(0);
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('name');
+            $table->string('image');
+            $table->string('description');
+            $table->string('CategoryID');
             $table->timestamps();
         });
     }
@@ -32,6 +30,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('movies');
     }
 };
