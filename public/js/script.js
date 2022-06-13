a6845e92c0f00d4b67472560bd420deab15fe8d1
ChiaHeung <D200262C@sc.edu.my>
2022-06-13 19:09:42 +0800

    Movie Recommendation + Genres System + Pagnitation

--- public/js/script.js
+++ public/js/script.js
@@ -10,17 +10,231 @@ const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&'+API_KEY;
 const IMG_URL = 'https://image.tmdb.org/t/p/w500';
 //Search
 const searchURL = BASE_URL + '/search/movie?'+API_KEY;
+//TMDb API Genres ID
+const genres = [
+    {
+      "id": 28,
+      "name": "Action"
+    },
+    {
+      "id": 12,
+      "name": "Adventure"
+    },
+    {
+      "id": 16,
+      "name": "Animation"
+    },
+    {
+      "id": 35,
+      "name": "Comedy"
+    },
+    {
+      "id": 80,
+      "name": "Crime"
+    },
+    {
+      "id": 99,
+      "name": "Documentary"
+    },
+    {
+      "id": 18,
+      "name": "Drama"
+    },
+    {
+      "id": 10751,
+      "name": "Family"
+    },
+    {
+      "id": 14,
+      "name": "Fantasy"
+    },
+    {
+      "id": 36,
+      "name": "History"
+    },
+    {
+      "id": 27,
+      "name": "Horror"
+    },
+    {
+      "id": 10402,
+      "name": "Music"
+    },
+    {
+      "id": 9648,
+      "name": "Mystery"
+    },
+    {
+      "id": 10749,
+      "name": "Romance"
+    },
+    {
+      "id": 878,
+      "name": "Science Fiction"
+    },
+    {
+      "id": 10770,
+      "name": "TV Movie"
+    },
+    {
+      "id": 53,
+      "name": "Thriller"
+    },
+    {
+      "id": 10752,
+      "name": "War"
+    },
+    {
+      "id": 37,
+      "name": "Western"
+    }
+  ]
+
+const main = document.getElementById('main');
+const form =  document.getElementById('form');
+const search = document.getElementById('search');
+const tagsEl = document.getElementById('tags');
+
+const prev = document.getElementById('prev')
+const next = document.getElementById('next')
+const current = document.getElementById('current')
+
+var currentPage = 1;
+var nextPage = 2;
+var prevPage = 3;
+var lastUrl = '';
+var totalPages = 100;
+
+  var selectedGenre = []
+  setGenre();
+  function setGenre() {
+      tagsEl.innerHTML= '';
+      genres.forEach(genre => {
+          const t = document.createElement('div');
+          t.classList.add('tag');
+          t.id=genre.id;
+          t.innerText = genre.name;
+          t.addEventListener('click', () => {
+              if(selectedGenre.length == 0){
+                  selectedGenre.push(genre.id);
+              }else{
+                  if(selectedGenre.includes(genre.id)){
+                      selectedGenre.forEach((id, idx) => {
+                          if(id == genre.id){
+                              selectedGenre.splice(idx, 1);
+                          }
+                      })
+                  }else{
+                      selectedGenre.push(genre.id);
+                  }
+              }
+              console.log(selectedGenre)
+              getMovies(API_URL + '&with_genres='+encodeURI(selectedGenre.join(',')))
+              highlightSelection()
+          })
+          tagsEl.append(t);
+      })
+  }
+  
+  function highlightSelection() {
+      const tags = document.querySelectorAll('.tag');
+      tags.forEach(tag => {
+          tag.classList.remove('highlight')
+      })
+      clearBtn()
+      if(selectedGenre.length !=0){   
+          selectedGenre.forEach(id => {
+              const hightlightedTag = document.getElementById(id);
+              hightlightedTag.classList.add('highlight');
+          })
+      }
+  
+  }
+  
+  function clearBtn(){
+      let clearBtn = document.getElementById('clear');
+      if(clearBtn){
+          clearBtn.classList.add('highlight')
+      }else{
+  
+          let clear = document.createElement('div');
+          clear.classList.add('tag','highlight');
+          clear.id = 'clear';
+          clear.innerText = 'Clear x';
+          clear.addEventListener('click', () => {
+              selectedGenre = [];
+              setGenre();            
+              getMovies(API_URL);
+          })
+          tagsEl.append(clear);
+      }
+  
+  }
 
 getMovies(API_URL);
 
 function getMovies(url) {
+    lastUrl = url;
+      fetch(url).then(res => res.json()).then(data => {
+          console.log(data.results)
+          if(data.results.length !== 0){
+              showMovies(data.results);
+              currentPage = data.page;
+              nextPage = currentPage + 1;
+              prevPage = currentPage - 1;
+              totalPages = data.total_pages;
+  
+              current.innerText = currentPage;
+  
+              if(currentPage <= 1){
+                prev.classList.add('disabled');
+                next.classList.remove('disabled')
+              }else if(currentPage>= totalPages){
+                prev.classList.remove('disabled');
+                next.classList.add('disabled')
+              }else{
+                prev.classList.remove('disabled');
+                next.classList.remove('disabled')
+              }
+  
+              tagsEl.scrollIntoView({behavior : 'smooth'})
+  
+          }else{
+              main.innerHTML= `<h1 class="no-results">No Results Found</h1>`
+          }
+         
+      })
+  
+  }
 
-    fetch(url).then(res => res.json()).then(data => {
-        console.log(data.results)
-        showMovies(data.results);
-    })
-
-}
+prev.addEventListener('click', () => {
+    if(prevPage > 0){
+      pageCall(prevPage);
+    }
+  })
+  
+  next.addEventListener('click', () => {
+    if(nextPage <= totalPages){
+      pageCall(nextPage);
+    }
+  })
+  
+  function pageCall(page){
+    let urlSplit = lastUrl.split('?');
+    let queryParams = urlSplit[1].split('&');
+    let key = queryParams[queryParams.length -1].split('=');
+    if(key[0] != 'page'){
+      let url = lastUrl + '&page='+page
+      getMovies(url);
+    }else{
+      key[1] = page.toString();
+      let a = key.join('=');
+      queryParams[queryParams.length -1] = a;
+      let b = queryParams.join('&');
+      let url = urlSplit[0] +'?'+ b
+      getMovies(url);
+    }
+  } 
 
 function showMovies(data) {
     main.innerHTML = '';
@@ -30,7 +244,7 @@ function showMovies(data) {
         const movieEl = document.createElement('div');
         movieEl.classList.add('movie');
         movieEl.innerHTML = `
-             <img src="${IMG_URL+poster_path}" alt="${title}">
+        <img src="${poster_path? IMG_URL+poster_path: "http://via.placeholder.com/1080x1580" }" alt="${title}">
             <div class="movie-info">
                 <h3>${title}</h3>
                 <span class="${getColor(vote_average)}">${vote_average}</span>
--- resources/views/movieRecommendation.blade.php
+++ resources/views/movieRecommendation.blade.php
@@ -8,54 +8,15 @@
 <body><br><br>
 <header>
         <form  id="form">
-            <input type="text" placeholder="Search" id="search" class="search">
+            <input type="text" placeholder="Search for your movie" id="search" class="search">
         </form>
     </header>
-    <main id="main">
-        <div class="movie">
-            <img src="https://images.unsplash.com/photo-1518173835740-f5d14111d76a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=877&q=80" alt="Image">
-
-            <div class="movie-info">
-                <h3>Movie Title</h3>
-                <span class="green">9.8</span>
-            </div>
-
-            <div class="overview">
-
-                <h3>Overview</h3>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab laudantium accusamus consequuntur explicabo numquam, totam quia illum dicta? Quod, vitae?
-            </div>
-        </div>
-
-        <div class="movie">
-            <img src="https://images.unsplash.com/photo-1518173835740-f5d14111d76a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=877&q=80"
-                alt="Image">
-
-            <div class="movie-info">
-                <h3>Movie Title</h3>
-                <span class="green">9.8</span>
-            </div>
-
-            <div class="overview">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab laudantium accusamus consequuntur explicabo numquam,
-                totam quia illum dicta? Quod, vitae?
-            </div>
-        </div>
-        <div class="movie">
-            <img src="https://images.unsplash.com/photo-1518173835740-f5d14111d76a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=877&q=80"
-                alt="Image">
-
-            <div class="movie-info">
-                <h3>Movie Title</h3>
-                <span class="green">9.8</span>
-            </div>
-
-            <div class="overview">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab laudantium accusamus consequuntur explicabo numquam,
-                totam quia illum dicta? Quod, vitae?
-            </div>
-        </div>
-    </main>
-
-    <script src="script.js"></script>
+    <div id="tags"></div>
+    <main id="main"></main>
+    <div class="pagination">
+        <div class="page" id="prev"> << Previous Page </div>
+        <div class="current" id="current">1</div>
+        <div class="page" id="next">Next Page >></div>
+    </div>
 </body>
+
