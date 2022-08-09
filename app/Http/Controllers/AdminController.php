2eb6414bfc9a2c9c2b62aa06b028fd16907aade1
TehJunYuan <80732651+TehJunYuan@users.noreply.github.com>
2022-08-09 19:02:29 +0800

    Update AdminController.php

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -12,11 +12,6 @@ use Session;
 class AdminController extends Controller
 {
     //
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-    
     public function showAdmin(){
         $admins = DB::table('users')
         ->where('is_admin','=','1')
