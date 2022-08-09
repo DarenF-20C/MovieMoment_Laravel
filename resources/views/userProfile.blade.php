f4b0c51af0dbcedbbc02d07ca61611385726439a
TehJunYuan <80732651+TehJunYuan@users.noreply.github.com>
2022-08-09 16:54:31 +0800

    Update userProfile.blade.php

--- resources/views/userProfile.blade.php
+++ resources/views/userProfile.blade.php
@@ -1,6 +1,6 @@
 @extends('layout')
 @section('content')
-<body>
+<body style="min-height:480px;">
     
     <br><br>
     <title>User Profile</title>
@@ -32,7 +32,7 @@
                     </a>
                   </li>
                   <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
-                    <a href="{{ route('editPassword',['id'=>$user->id])}}" style="color:black;">
+                    <a href="{{ route('editPassword')}}" target="__blank" style="color:black;">
                     <h6 class="mb-0"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-lock"><rect x="3" y="11" width="18" height="11" rx="2" ry="2"></rect><path d="M7 11V7a5 5 0 0 1 10 0v4"></path></svg>  Password</h6>
                     <span class="text-secondary"></span>
                   </a>
@@ -47,7 +47,7 @@
                     <div class="col-sm-3">
                       <h6 class="mb-0">Full Name</h6>
                     </div>
-                    <div class="col-sm-9 text-secondary">
+                    <div class="col-sm-9 text-dark">
                     {{$user->name}}
                     </div>
                   </div>
@@ -56,7 +56,7 @@
                     <div class="col-sm-3">
                       <h6 class="mb-0">Email</h6>
                     </div>
-                    <div class="col-sm-9 text-secondary">
+                    <div class="col-sm-9 text-dark">
                     {{$user->email}}
                     </div>
                   </div>
@@ -65,7 +65,7 @@
                     <div class="col-sm-3">
                       <h6 class="mb-0">Phone</h6>
                     </div>
-                    <div class="col-sm-9 text-secondary">
+                    <div class="col-sm-9 text-dark">
                     {{$user->phone}}
                     </div>
                   </div>
@@ -74,7 +74,7 @@
                     <div class="col-sm-3">
                       <h6 class="mb-0">Gender</h6>
                     </div>
-                    <div class="col-sm-9 text-secondary">
+                    <div class="col-sm-9 text-dark">
                     {{$user->gender}}
                     </div>
                   </div>
@@ -83,14 +83,14 @@
                     <div class="col-sm-3">
                       <h6 class="mb-0">Date of Birth</h6>
                     </div>
-                    <div class="col-sm-9 text-secondary">
+                    <div class="col-sm-9 text-dark1">
                     {{$user->date}}
                     </div>
                   </div>
                   <hr>
                   <div class="row">
                     <div class="col-sm-12">
-                      <a class="btn btn-info " target="__blank" href="{{ route('editProfile',['id'=>$user->id])}}" style="item-align:right;">Edit Profile</a>
+                      <a class="btn btn-info " target="__blank" href="{{ route('editProfile')}}" style="item-align:right;">Edit Profile</a>
                     </div>
                   </div>
                 </div>
@@ -101,13 +101,8 @@
                   <div class="card h-100">
                     <div class="card-body">
                       <h6 class="d-flex align-items-center mb-3"><i class="material-icons text-info mr-2">Total Post: </i>{{$totalPost}}</h6>
-                    </div>
-                  </div>
-                </div>
-                <div class="col-sm-12 mb-3">
-                  <div class="card h-100">
-                    <div class="card-body">
                       <h6 class="d-flex align-items-center mb-3"><i class="material-icons text-info mr-2">Total Comment: </i>{{$totalComment}}</h6>
+                      <h6 class="d-flex align-items-center mb-3"><i class="material-icons text-info mr-2">Total Point: </i>{{$user->point}}</h6>
                     </div>
                   </div>
                 </div>
